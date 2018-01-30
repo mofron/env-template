3660880fb4a05e8104704e3b77076339821738cc
root <satoki.taira@g.softbank.co.jp>
2018-01-30 19:07:50 +0900

    add

--- tool/init.sh
+++ tool/init.sh
@@ -2,9 +2,41 @@
 SCP_DIR=$(cd $(dirname $0);pwd);
 cd $SCP_DIR/../
 
-sudo rm LICENSE
-sudo rm README.md
-sudo rm -rf ./git
-sudo rm ./tool/init.sh
+error () {
+    echo "ERROR : $1"
+    echo "init.sh is failed"
+    exit -1
+}
+
+del_file () {
+    sudo rm LICENSE
+    if [ $? != 0 ]; then
+        error "could not delete LICENSE"
+    fi
+
+    sudo rm README.md
+    if [ $? != 0 ]; then
+        error "could not delete README.md"
+    fi
+    
+    sudo rm -rf ./git
+    if [ $? != 0 ]; then
+        error "could not delete ./git"
+    fi
+    
+    sudo rm ./tool/init.sh
+    if [ $? != 0 ]; then
+        error "could not delete init.sh"
+    fi
+}
+
+build_env () {
+    sudo yum install -y epel-release
+    sudo yum install -y nodejs npm
+}
+
+del_file
+build_env
+echo `./tool/build.sh`
 
 echo "initialized mofron env"
