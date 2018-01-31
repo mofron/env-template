c0c90df9c0a965366f4abe7c4a1315c1ff8fd00b
root <aaa>
2018-01-31 16:54:30 +0900

    move init dir

--- conf/webpack/webpack.config.index.js
+++ conf/webpack/webpack.config.index.js
@@ -1,6 +1,6 @@
 const webpack =　require("webpack");
 module.exports = {
-    entry: __dirname + '/../../src/js/ctrl/init/index.js',
+    entry: __dirname + '/../../src/js/init/index.js',
     output: {
         path: __dirname + '/../../src/js/app',
         filename: 'index.js'
