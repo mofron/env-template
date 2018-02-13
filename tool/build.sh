ce98ac2bd589c77fd1ecb425090a14dcd4005e13
root <aaa>
2018-02-13 15:29:22 +0900

    delete file

--- tool/build.sh
+++ tool/build.sh
@@ -3,5 +3,4 @@ SCP_DIR=$(cd $(dirname $0);pwd);
 cd $SCP_DIR/../
 WEBPACK=node_modules/.bin/webpack
 
-#echo $($WEBPACK --config conf/webpack/webpack.config.index.js);
-echo $($WEBPACK --config conf/webpack/webpack.config.login.js );
+echo $($WEBPACK --config conf/webpack/webpack.config.index.js);
