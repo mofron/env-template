5d593dc6fb45f8a2b23f456175f352ea32683a5e
root <aaa>
2018-02-26 16:29:23 +0900

    enable webpack

--- conf/webpack/webpack.config.index.js
+++ conf/webpack/webpack.config.index.js
@@ -1,21 +1,27 @@
 const webpack =　require("webpack");
 module.exports = {
-    entry: __dirname + '/../../src/js/init/index.js',
-    output: {
-        path: __dirname + '/../../src/js/app',
-        filename: 'index.js'
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ],
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: __dirname + '../../node_modules/',
-            loader: 'babel-loader'  ,
-            query: {
-                presets: ['env']
-            }
-        }]
-    }
+  entry: __dirname + '/../../src/js/init/index.js',
+  output: {
+      path: __dirname + '/../../src/js/app',
+      filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: __dirname + '../../node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]
 };
