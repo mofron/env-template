6ca57c6adc372c2d4fc18a5b64dc5d15de2c83a1
simpart <simpart>
2018-04-18 11:22:00 +0900

    splice script

--- tool/init/centos.sh
+++ tool/init/centos.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SCP_DIR=$(cd $(dirname $0);pwd);
-cd $SCP_DIR/../../
 
 error () {
     echo "ERROR : $1"
@@ -8,41 +7,10 @@ error () {
     exit -1
 }
 
-del_file () {
-    sudo rm LICENSE
-    if [ $? != 0 ]; then
-        error "could not delete LICENSE"
-    fi
+echo "*** start init centos"
 
-    sudo rm README.md
-    if [ $? != 0 ]; then
-        error "could not delete README.md"
-    fi
-    
-    sudo rm -rf ./git
-    if [ $? != 0 ]; then
-        error "could not delete ./git"
-    fi
-}
+bash $SCP_DIR/deltemp.sh
+bash $SCP_DIR/centos/node.sh
+bash $SCP_DIR/mofron.sh
 
-build_env () {
-    sudo yum install -y epel-release
-    sudo yum install -y nodejs npm
-}
-
-del_file
-build_env
-npm install mofron
-if [ $? != 0 ]; then
-    error "could not install mofron"
-fi
-echo `./tool/build.sh`
-
-rm -rf ./src/js/app/.dmy
-rm -rf ./src/js/comp/.dmy
-rm -rf ./html/.dmy
-rm -rf ./img/.dmy
-rm -rf ./font/.dmy
-rm -rf .git/
-
-echo "initialized mofron env"
+echo "*** successful init centos"
