ebf63ccff9abd2b64a2fcf69873ba7c24e5b5d30
simpart <simpart>
2018-08-29 00:31:31 +0000

    add tagbuild

--- tool/build.sh
+++ tool/build.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 SCP_DIR=$(cd $(dirname $0);pwd);
-cd $SCP_DIR/../
-WEBPACK=node_modules/.bin/webpack
-MODE=''
+WEBPACK="${SCP_DIR}/../node_modules/.bin/webpack"
 
-if [[ $1 = "pro" ]]; then
+error () {
+    echo "ERROR : $1"
+    echo "Usage:"
+    echo "  build.sh [conf] [mode]"
+    echo "    conf : path to config file path"
+    echo "    mode : 'pro', 'dev'"
+    exit -1
+}
+
+# get target file
+if [[ $1 == "" ]]; then
+    TARGET="$SCP_DIR/../conf/webpack/webpack.config.index.js"
+else
+    TARGET="$SCP_DIR/../conf/webpack/webpack.config.$1.js"
+fi
+if [ ! -f $TARGET ]; then
+    error "could not found webpack config file (${TARGET})"
+fi
+
+# get mode
+if [[ $2 = "pro" ]]; then
   MODE='production'
 else
   MODE='development'
 fi
 
-EXEC="$WEBPACK --config conf/webpack/webpack.config.index.js --mode $MODE"
+
+EXEC="$WEBPACK --config ${TARGET} --mode $MODE"
 echo $EXEC
 echo $($EXEC);
-#echo $($WEBPACK --config conf/webpack/webpack.config.index.js --mode $MODE);
